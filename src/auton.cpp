f1c0cb68be9ec39e26784244f0cf7a156052f216
katarafett <arpperry19@gmail.com>
2023-11-04 19:52:27 -0400

    post-tourney 1

--- src/auton.cpp
+++ src/auton.cpp
@@ -1,10 +1,13 @@
 #include "../include/main.h"
 
+void release_antenna(void);
+
 void autonomous(void) {
     switch (auton_mode) {
         case AWP:
             break;
         case HALF_AWP_NEAR:
+            /* release_antenna();
             // Get to triball
             drive_straight(-15, 24, 48);
             // Remove triball
@@ -12,8 +15,30 @@ void autonomous(void) {
             drive_turn(35, WHEEL_TO_WHEEL_DIST / 2, 36, 48, false);
             // Reset
             balance.set(0);
+            // Try AWP
+            wait(200, vex::msec);
+            drive_turn(-45, WHEEL_TO_WHEEL_DIST / 2, 36, 36, true);
+            drive_straight(26, 48, 48);
+            drive_turn(-42, WHEEL_TO_WHEEL_DIST / 2, 36, 36, true);
+            drive_straight(24, 24, 24);
+            break; */
+
+            // Elims
+            drive_straight(-28, 48, 48);
+            drive_turn(15, WHEEL_TO_WHEEL_DIST - 9, 36, 36, false);
+            drive_straight(3, 24, 24);
+            wait(200, vex::msec);
+            drive_l.spin(DIR_FWD, -100, VEL_PCT);
+            drive_r.spin(DIR_FWD, -100, VEL_PCT);
+            wait(1000, vex::msec);
+            drive_straight(15, 48, 48);
+            drive_turn(90, WHEEL_TO_WHEEL_DIST / 2, 36, 36, false);
+            drive_straight(-4, 24, 24);
+            drive_turn(-4, WHEEL_TO_WHEEL_DIST - 10, 36, 36, true);
+            drive_straight(2, 24, 24);
             break;
         case HALF_AWP_FAR:
+            release_antenna();
             drive_straight(-28, 48, 48);
             drive_turn(-15, WHEEL_TO_WHEEL_DIST - 9, 36, 36, true);
             drive_straight(3, 24, 24);
@@ -22,12 +47,20 @@ void autonomous(void) {
             drive_r.spin(DIR_FWD, -100, VEL_PCT);
             wait(1000, vex::msec);
             drive_straight(12, 48, 48);
-            drive_l.spin(DIR_FWD, -100, VEL_PCT);
-            drive_r.spin(DIR_FWD, -100, VEL_PCT);
-            wait(1000, vex::msec);
-            drive_straight(12, 48, 48);
+            // Try AWP
+            drive_turn(38, WHEEL_TO_WHEEL_DIST / 2, 36, 36, false);
+            drive_straight(32, 48, 48);
+            drive_turn(41, WHEEL_TO_WHEEL_DIST / 2, 36, 36, false);
+            drive_straight(25, 24, 24);
             break;
         case SKILLS:
+            cata.spin(DIR_FWD, 50, VEL_PCT);
             break;
     }
+}
+
+void release_antenna(void) {
+    intake.spin(DIR_FWD, 75, VEL_PCT);
+    wait(500, vex::msec);
+    intake.stop();
 }
--- src/globals.cpp
+++ src/globals.cpp
@@ -1,6 +1,6 @@
 #include "../include/main.h"
 
 // Autonomous
-int auton_mode = HALF_AWP_FAR;
+int auton_mode = HALF_AWP_NEAR;
 float target_heading = 0;
 float fly_rpm = 2000;
